26b40c86ce2edca1255cfbc0b780699653e5b4c9
LuisADR <luisangel.diaz188@gmail.com>
2019-08-26 07:22:35 -0500

    Class Code

---
+++ DataStructure/Bidimencionales/Arreglo8.java
@@ -0,0 +1,80 @@
+import javax.swing.JOptionPane;
+
+public class Arreglo8{
+
+  private int numeros1[][], numeros2[][], resultado[][];
+  private int renglones, columnas;
+
+  private String desplegarDatos(int array[][], String msg){
+    String resultado = msg;
+
+    for (int i = 0; i < renglones; i++) {
+      resultado = resultado + "\n";
+      for (int j = 0;j < columnas; j++) {
+        resultado = resultado + " " + array[i][j];
+      }
+    }
+
+    return resultado;
+  }
+
+  private void obtenerDatos(int array[][]){
+
+    for (int i = 0; i < renglones; i++) {
+      for (int j = 0;j < columnas; j++) {
+        array[i][j] = Integer.parseInt(JOptionPane.showInputDialog("Casilla["+i+"]["+j+"]"));
+      }
+    }
+  }
+
+  private void suma(int array1[][], int array2[][]){
+    for (int i = 0; i < renglones; i++) {
+      for (int j = 0;j < columnas; j++) {
+        resultado[i][j] = array1[i][j] + array2[i][j];
+      }
+    }
+  }
+
+  private void multiplicacion(int array1[][], int array2[][]){
+
+  }
+
+  private void principal(){
+
+    // 1. Obtener el no. de casillas del arreglo
+    String strN = JOptionPane.showInputDialog("No. de renglones");
+    renglones = Integer.parseInt(strN);
+
+    strN = JOptionPane.showInputDialog("No. de columnas");
+    columnas = Integer.parseInt(strN);
+
+    // 2. Crear el arreglo en RAM
+    numeros1 = new int[renglones][columnas];
+    numeros2 = new int[renglones][columnas];
+    resultado = new int[renglones][columnas];
+
+    // 3. Obtener datos y asignarlos al arreglo
+    obtenerDatos(numeros1);
+
+    // 4. Desplegar los datos del arreglo
+    JOptionPane.showMessageDialog(null, desplegarDatos(numeros1, "Primera matriz \n"));
+
+    // 3. Obtener datos y asignarlos al arreglo
+    obtenerDatos(numeros2);
+
+    // 4. Desplegar los datos del arreglo
+    JOptionPane.showMessageDialog(null, desplegarDatos(numeros2, "Segunda matriz \n"));
+
+    // Multiplicar matices
+    suma(numeros1, numeros2);
+
+    // Desplegar los datos del arreglo
+    JOptionPane.showMessageDialog(null, desplegarDatos(resultado, "Resultado \n"));
+
+  }
+
+  public static void main(String[] args) {
+    Arreglo8 objeto = new Arreglo8();
+    objeto.principal();
+  }
+}
