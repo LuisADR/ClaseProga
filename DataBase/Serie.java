2cc993e90948e607a1d189959fdec1293fd3fe57
Luis Angel <luisangel.diaz188@gmail.com>
2019-08-26 00:28:36 -0500

    Add

---
+++ DataBase/Serie.java
@@ -0,0 +1,15 @@
+class Serie{
+  public static void main(String args[]){
+    int x = Integer.parseInt(args[0]);
+    int resultado = 1+x
+    for(int i = 0; i < args[1]; i++){
+
+    }
+  }
+
+  private int factorial(int valor){
+    for (int i = 1; i <= valor+1; i++ ) {
+      
+    }
+  }
+}
