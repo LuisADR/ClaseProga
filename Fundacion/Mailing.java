cf71d4a9e3df9b4b7240a5d8ccbf4e93947ec122
Luis Angel <luisangel.diaz188@gmail.com>
2019-11-07 07:07:12 -0600

    Fundacion Tlaloc

---
+++ Fundacion/Mailing.java
@@ -0,0 +1,49 @@
+//SET CLASSPATH=C:\Users\churr\Documents\GitHub\ClaseProga\Fundacion\javax.mail.jar;.;%CLASSPATH%
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Mailing {
+
+    public static void main(String[] args) {
+
+        final String username = "luisangel.diaz188@gmail.com";
+        final String password = "qqxwifgyjarmtywh";
+
+        Properties prop = new Properties();
+		    prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("luisangel.diaz188@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("a01365832@itesm.mx")
+            );
+            message.setSubject("Correo de Prueba");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n Please do not spam my email!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
